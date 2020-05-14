506ac21196d265e7e6574ffe35fff56e00ec3858
danilofatcat <danilo.gasevic@fatcatcoders.com>
2020-05-14 13:39:44 +0200

    fix

--- src/modules/time/time.service.ts
+++ src/modules/time/time.service.ts
@@ -17,7 +17,6 @@ export class TimeService extends BaseService{
 	 * Find all user activities sorted by descending
 	 * @async
 	 * @param userId - id of the user
-	 * @returns {[Time]}
 	 */
 	async getTimesFromUserIdDescending(userId: string) : Promise<Time[]> {
 		return this.timeModel.findAll({
@@ -37,7 +36,6 @@ export class TimeService extends BaseService{
 	 * Find particular activity
 	 * @async
 	 * @param id - id of the timer
-	 * @returns {Time}
 	 */
 	async getTimeFromId(id) : Promise<Time>{
 		return this.timeModel.findOne({
